2b82c1377d230fceeb4a71f68f6e0c33ead92494
吴昊 <mrw0628@163.com>
2022-10-04 19:11:46 +0800

    Initial commit

---
+++ src/main/java/com/wuhao/springbootStudy/SpringBootStudyApplication.java
@@ -0,0 +1,14 @@
+package com.wuhao.springbootStudy;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * SpringBoot的应用启动类
+ */
+public class SpringBootStudyApplication {
+    public static void main(String[] args) {
+        // 1. 通过静态 run 方法
+        SpringApplication.run(SpringBootStudyApplication.class, args) ;
+    }
+
+}
